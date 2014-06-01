60d909463f715635286516f7cd2f4c313a681abd
nttdocomo <nttdocomo.ouyi@gmail.com>
2014-06-01 14:38:24 +0800

    add product url

---
+++ urls.product.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014-1-23
+
+@author: nttdocomo
+'''
+import web
+from auth.controllers import *
+from api.urls import app_api
+from sessions.models import Session, SQLAStore
+
+web.config.smtp_server = 'smtp.gmail.com'
+web.config.smtp_port = 587
+web.config.smtp_username = 'nttdocomo.ouyi@gmail.com'
+web.config.smtp_password = '709394803610'
+web.config.smtp_starttls = True
+
+urls = (
+    '/api', app_api,
+    '/register/', 'RegisterPage',
+    '/activate/(\w+)/','Activate',
+    '/admin/', 'Admin',
+    '/admin/login/', 'Login',
+    '/(.*)', 'hello'
+)
+app = web.application(urls, locals())
+
+if web.config.get('session') is None:
+    web.config.session = session = Session(app, SQLAStore())
+else:
+    session = web.config.session
+
+if __name__ == "__main__":
+    #关掉stderr的输出位置
+    import sys
+    sys.stderr = None
+    #fastcgi的方式运行app
+    web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+    app.run()
