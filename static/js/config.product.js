ec0f640a706ea78e57754c066627404b294a034a
nttdocomo <nttdocomo.ouyi@gmail.com>
2014-06-06 20:57:54 +0800

    use absolute url instead relative url in config base

--- static/js/config.product.js
+++ static/js/config.product.js
@@ -3,14 +3,14 @@
  */
 seajs.config({
 	plugins : ['shim', 'text'],
-	base : "/static/js/",
+	base : 'http://'+location.host+'/static/js/taurus/',
 	alias : {
 		"jquery" : "jquery-2.0.0.js",
 		"underscore" : "underscore.js",
 		"modernizr" : "taurus/modernizr",
 		"moment" : "taurus/moment"
 	},
-	map : [['http://www.kickeleven.com/static/js/taurus/', 'http://nttdocomo.github.io/taurus/src/']],
+	map : [['http://'+location.host+'/static/js/taurus/', 'http://nttdocomo.github.io/taurus/src/']],
 	preload : ['jquery', 'underscore', 'modernizr', 'plugin-text'],
 	vars : {
 		'locale' : (navigator.language || navigator.browserLanguage).toLowerCase()
