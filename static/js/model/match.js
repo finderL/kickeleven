fae808a0d415768d9c843954fdea042543d01d83
nttdocomo <nttdocomo.ouyi@gmail.com>
2014-06-14 15:53:04 +0800

    update match model

--- static/js/model/match.js
+++ static/js/model/match.js
@@ -5,10 +5,17 @@ define(function(require) {
 	return Backbone.Model.extend({
 		url:function(){
 			if(this.isNew()){
-				return '/api/match?' + (this.isAdmin ? '&admin=1':'');
+				return '/api/match/' + (this.isAdmin ? '?admin=1':'');
 			} else {
 				return '/api/match/?id=' + this.id + (this.isAdmin ? '&admin=1':'');
 			}
+		},
+		parse : function(resp) {
+			//if(resp.results && resp.results)
+			if(resp.rv){
+				return resp.rv.match;
+			}
+			return resp;
 		}
 	});
 });
