92e47bdce43a294cfda2fc91a15dbccb154c46cf
nttdocomo <nttdocomo.ouyi@gmail.com>
2014-07-28 21:10:31 +0800

    add 1.Bundesliga and template update

--- static/js/config.product.js
+++ static/js/config.product.js
@@ -5,7 +5,6 @@ seajs.config({
 	plugins : ['shim', 'text'],
 	base : 'http://'+location.host+'/static/js/',
 	alias : {
-		"jquery" : "jquery-2.0.0.js",
 		"underscore" : "underscore.js",
 		"modernizr" : "http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js",
 		"backbone" : "http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js",
@@ -13,7 +12,7 @@ seajs.config({
 		"moment" : "http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js"
 	},
 	map : [['http://'+location.host+'/static/js/taurus/', 'http://nttdocomo.github.io/taurus/src/']],
-	preload : ['jquery', 'underscore', 'modernizr', 'plugin-text'],
+	preload : ['plugin-text'],
 	vars : {
 		'locale' : (navigator.language || navigator.browserLanguage).toLowerCase()
 	},
