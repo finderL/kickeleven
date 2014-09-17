a7532871c756bd2de38f84126e426fbacefc4435
nttdocomo <nttdocomo.ouyi@gmail.com>
2014-09-17 18:08:42 +0800

    use pool to test the mysql error

--- db/models.py
+++ db/models.py
@@ -9,9 +9,27 @@ import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import exc
+from sqlalchemy import event
+from sqlalchemy.pool import Pool
+
+@event.listens_for(Pool, "checkout")
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("SELECT 1")
+    except:
+        # optional - dispose the whole pool
+        # instead of invalidating one at a time
+        # connection_proxy._pool.dispose()
+
+        # raise DisconnectionError - pool will try
+        # connecting again up to three times before raising.
+        raise exc.DisconnectionError()
+    cursor.close()
 
 DB_CONNECT_STRING = 'mysql+mysqldb://%s%s@localhost/%s?charset=utf8' % (settings.DATABASE_USERNAME, settings.DATABASE_PASSWORD, settings.DATABASE_NAME)
-engine = create_engine(DB_CONNECT_STRING, echo=True)
+engine = create_engine(DB_CONNECT_STRING, echo_pool=True)
 DB_Session = sessionmaker(bind=engine,expire_on_commit=False)
 sql_session = scoped_session(sessionmaker(bind=engine))
 
